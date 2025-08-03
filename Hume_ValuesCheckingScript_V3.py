fb14105d49e29f5cf165edb4f24dc1019fba3ccc
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2025-08-03 15:41:03 +1000

    Update Hume_ValuesCheckingScript_V3.py

--- Hume_ValuesCheckingScript_V3.py
+++ Hume_ValuesCheckingScript_V3.py
@@ -13,22 +13,22 @@ from datetime import datetime
 from typing import List, Tuple, Any
 import csv
 
-#####################################################################################
-#           ADJUST THESE IF RUNNING OUTSIDE OF THE ARCGIS PRO TOOLBOX               #
-#####################################################################################
-
-# Manual parameters - script will use these if arcpy.GetParameterAsText isn't found
+#######################################################################################
+#            ADJUST THESE IF RUNNING OUTSIDE OF THE ARCGIS PRO TOOLBOX                #
+#######################################################################################
+"""Manual parameters - script will use these if arcpy.GetParameterAsText isn't found"""
 FEATURE_CLASS   = 'C:\\data\\daptest\\Hume_uploadtoVDP_20250718.gdb\\DAP_FINAL_AREA_20250718'             # Input Feature Class
 # FEATURE_CLASS   = 'C:\\data\\daptest\\Hume_uploadtoVDP_20250718_short.shp'
-FEATURE_ID      = 'DAP_REF_NO'                                  # Feature ID Field
+FEATURE_ID      = 'DAP_REF_NO'                        # Feature ID Field
 THEME_REFTAB    = 'C:\\data\\daptest\\Single Report Tool\\Reference Tables\\reftables.gdb\\REFTABLE_DAP_20250417'  # Theme Reference Table
 # THEME_REFTAB    = 'C:\\data\\daptest\\Single Report Tool\\Reference Tables\\reftables.gdb\\REFTABLE_INDIGENOUS_HERITAGE_ALL_RECORDS'  # Theme Reference Table
-GISPUB_PATH     = 'C:\\data'                                    # Local gis_public folder location
-OUT_PATH        = 'C:\\data\\20250709_hume_test'                # Output Path
-SPATIAL_REF     = 7899                                          # Set spatial reference to VICGRID2020
+GISPUB_PATH     = 'C:\\data'                          # gis_public folder location
+OUT_PATH        = 'C:\\data\\20250709_hume_test'      # Output Path
+SPATIAL_REF     = 7899                                # Set spatial ref to VICGRID2020
+MAX_STRING_LEN  = 50
 
-######################################################################################
-######################################################################################
+#######################################################################################
+#######################################################################################
 
 class ValuesCheckTool:
     """Main class for performing spatial values checking operations."""
@@ -354,8 +354,8 @@ class ValuesCheckTool:
                         
                         # skip if empty or any variety of no-value
                         if str_val and str_val.lower() not in ['none', 'null', 'nan', '']:
-                            # reduce to max 50 characters and add to list field data 
-                            field_values.append(str_val[:50])
+                            # reduce to max characters and add to list field data 
+                            field_values.append(str_val[:MAX_STRING_LEN])
                         
                     # add reporting fields (plus count/measure if req.) to output dictionar, as a LIST                    
                     if method.upper() == "PRESENT":
